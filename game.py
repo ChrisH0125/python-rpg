b3e8cb02cfae925f994033a2a069b3527a3cbf30
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-22 22:18:00 -0400

    Finished dagger enemy 1

--- game.py
+++ game.py
@@ -11,9 +11,9 @@ enemy5hp = 100
 randomAtk = random.randInt(5,30)
 superBlock = 30
 heal = health+=20
-sword = 0
-dagger = 0
-greatsword = 0
+sword = 10
+dagger = 5
+greatsword = 15
 block = 10
 print("Hello! Welcome to my first python project, it's basically a little RPG game where you have 3 enemies to defeat and a powerup after defeating each one!, have fun!!!\n")
 
@@ -24,4 +24,7 @@ if weapon_Prompt == 1:
     print("Your first choice is a dagger! It has 5 attack damage and your first enemy has 10 health!\n")
     attack_Prompt = int(input("Which attack do you want to use? \n\t1) Attack \n\t Block"))
     if(attack_Prompt == 1):
+        enemy1hp -= dagger
+        if(enemy1hp == 0):
+            power_Up1 = int(input("You have slain the first boss! What would you like to be your powerup? \n\t1) Random Attack Damage \n\t Super Block for 1 Turn \n\t Heal for 20 health\n"))
         
