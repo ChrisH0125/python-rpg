1c795a928d3c59a8a0ef28842c3962f15f4ff354
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-22 21:56:50 -0400

    Added variables for health and attack

--- game.py
+++ game.py
@@ -1,4 +1,12 @@
 import random
 
+
+health = 100
+attack = 1
+enemy1hp = 10
+enemy2hp = 20
+enemy3hp = 50
+enemy4hp = 70
+enemy5hp = 100
 intro_Dialogue= input("Hello! Welcome to a RPG-based game that has about 5 enemies to defeat! ;p have fun!")
 
