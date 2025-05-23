df510fc64213e2a00ddbe87b2573c2cb44d415e5
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-22 22:19:54 -0400

    Changed enemy count

--- game.py
+++ game.py
@@ -5,14 +5,13 @@ health = 100
 attack = 1
 enemy1hp = 10
 enemy2hp = 20
-enemy3hp = 50
 randomAtk = random.randInt(5,30)
 superBlock = 30
 sword = 10
 dagger = 5
 greatsword = 15
 block = 10
-print("Hello! Welcome to my first python project, it's basically a little RPG game where you have 3 enemies to defeat and a powerup after defeating each one!, have fun!!!\n")
+print("Hello! Welcome to my first python project, it's basically a little RPG game where you have 2 enemies to defeat and a powerup after defeating each one!, have fun!!!\n")
 
 name = input("So, let's get started! What is your name?: ")
 weapon_Prompt = int(input(("Welcome," + name + "!\n What do you want your weapon to be? \n\t1) Dagger \n\t2) Sword \n\t3) Greatsword\n")))
@@ -24,4 +23,5 @@ if weapon_Prompt == 1:
         enemy1hp -= dagger
         if(enemy1hp == 0):
             power_Up1 = int(input("You have slain the first boss! What would you like to be your powerup? \n\t1) Random Attack Damage \n\t Super Block for 1 Turn \n\t Heal for 20 health\n"))
+
         
