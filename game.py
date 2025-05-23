bbbfc4aaeababc88d4dc83fc27b685d463a271fc
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-22 22:25:17 -0400

    Added a while loop

--- game.py
+++ game.py
@@ -21,7 +21,12 @@ if weapon_Prompt == 1:
     attack_Prompt = int(input("Which attack do you want to use? \n\t1) Attack \n\t Block"))
     if(attack_Prompt == 1):
         enemy1hp -= dagger
-        if(enemy1hp == 0):
-            power_Up1 = int(input("You have slain the first boss! What would you like to be your powerup? \n\t1) Random Attack Damage \n\t Super Block for 1 Turn \n\t Heal for 20 health\n"))
+        while enemy1hp != 0:
+            health -= 20
+            try_Again1 = int(input("You didn't kill him my brotha, now your health is 80, would you wanna attack again? \n\t1) Yes \n\t2) No"))
+            if try_Again1 == 1:
+                enemy1hp -=dagger
+            else:
+                print("You suck")
 
         
