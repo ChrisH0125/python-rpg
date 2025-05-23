430f21896cb30d062e66a1210d20d9957ff4a5fc
ChrisH0125 <ho.chris.cs@gmail.com>
2025-05-22 22:06:29 -0400

    Edited print

--- game.py
+++ game.py
@@ -8,5 +8,5 @@ enemy2hp = 20
 enemy3hp = 50
 enemy4hp = 70
 enemy5hp = 100
-intro_Dialogue= input("Hello! Welcome to a RPG-based game that has about 5 enemies to defeat! ;p have fun!")
+print("Hello! Welcome to my first python project, it's basically a little RPG game where you have 5 enemies to defeat, have fun!!!\n")
 
